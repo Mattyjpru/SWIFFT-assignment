17840f89496101e24d1e57564cc8288b55ce5d87
RileyVaughn <rj.vaughn@live.com>
2023-02-16 14:17:36 -0700

    broken sieve

--- sieve.go
+++ sieve.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(SieveOfEratosthenes(101))
+}
+
+func SieveOfEratosthenes(n uint) []uint {
+
+	var isPrime []bool
+
+	for i := 0; i <= n; i++ {
+		isPrime[i] = false
+	}
+	isPrime[0] = true
+	isPrime[1] = true
+
+	for i, v := range isPrime {
+		if v {
+			for k := uint(2); uint(i)*k <= n; k++ {
+				isPrime[uint(i)*k] = true
+			}
+		}
+	}
+
+	var primes []uint
+	for p, _ := range isPrime {
+		primes = append(primes, uint(p))
+	}
+
+	return primes
 }
