7d083f4a9d64444cde6e25bb0d1c72b22a291a2e
Mattyjpru <98782573+Mattyjpru@users.noreply.github.com>
2023-03-16 14:36:21 -0600

    moved stuff around

--- sieve.go
+++ sieve.go
@@ -3,35 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(SieveOfEratosthenes(101))
+	fmt.Println(SieveOfEratosthenes(3))
 }
 
 func SieveOfEratosthenes(n uint) []uint {
-
-	var isPrime []bool
+	isPrime:=make([]bool, n+1)
 	
-	for i := 0; uint(i) <= n; i++ {
-		isPrime[i] = false
+	for i := 0; uint(i) <=n; i++ {
+		isPrime[i] = true
 	}
 	isPrime[0] = true
 	isPrime[1] = true
-
 	for j, v := range isPrime {
-		if v {
+		if v==true{
 			for k := uint(2); uint(j)*uint(k) <= n; k++ {
-				isPrime[uint(j)*k] = true
+				isPrime[uint(j)*k] = false
 			}
 		}
 	}
 
 	var primes []uint
 	for p, _ := range isPrime {
-		primes = append(primes, uint(p))
+		if(isPrime[p]==true){
+			primes = append(primes, uint(p))
+		}
+		
 	}
-
+//	TwinPrimes(primes)
 	return primes
 }
 
-//func TwinPrimes(n){
+//func TwinPrimes([]uint) []uint{
 
 //}
+
